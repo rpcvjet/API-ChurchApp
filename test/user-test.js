69d1c66a4787bf161769081f45a92478843d7db5
Kenneth Edwards <kennethedwards@Kenneths-MacBook-Pro.local>
2018-09-13 18:52:43 -0400

    testing string

--- test/user-test.js
+++ test/user-test.js
@@ -17,7 +17,7 @@ const serverControl = require('./lib/serverControl');
 describe('Testing user route', function() {
     
     before(serverControl.startServer)  
-    // after(serverControl.turnoffServer);
+    after(serverControl.turnoffServer);
     // after((done) => {
         //     User.remove({})
         //     .then( () => done())
